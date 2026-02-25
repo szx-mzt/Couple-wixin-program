aa4a2a8c9a8be72e2bc9958f74ab3e9b7914203c
师振鑫 <1557688927@qq.com>
2026-02-25 11:12:49 +0800

    去除私有云 ID

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -11,8 +11,8 @@ App({
       //   env 参数决定接下来小程序发起的云开发调用（wx.cloud.xxx）会默认请求到哪个云环境的资源
       //   此处请填入环境 ID, 环境 ID 可打开云控制台查看
       //   如不填则使用默认环境（第一个创建的环境）
-    //   env: "cloud1-5g9hjld9cb8fe986",
-      env: "max-2026-ban-4gxxli94aac23368"
+    //   env: "",
+      env: ""
     };
     if (!wx.cloud) {
       console.error("请使用 2.2.3 或以上的基础库以使用云能力");
